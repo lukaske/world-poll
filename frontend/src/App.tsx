dd8896a33898009a264de49ba2b8c5b87dd55d2d
Luka Skeledzija <36110818+lukaske@users.noreply.github.com>
2025-04-05 08:20:23 +0200

    install shadcn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import { PayBlock } from "./components/Pay";
 import { VerifyBlock } from "./components/Verify";
+import { Button } from "@/components/ui/button"
 
 export default function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-y-3">
       <VerifyBlock />
       <PayBlock />
+      <Button>Click me</Button>
+
     </main>
   );
 }
